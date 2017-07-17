0474aa829e6480bdfd3e0cbd41095fca2199f818
Rustem Khairutdinov <rustem-hairutdinov@mail.ru>
2017-07-17 10:21:27 +0300

    EyeExercise

--- VerneHelper/app/src/main/java/com/example/user/vernehelper/MainActivity.java
+++ VerneHelper/app/src/main/java/com/example/user/vernehelper/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.user.vernehelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (ImageView) findViewById(R.id.image_view_exeForEyes_main);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EyeSettings.class));
+            }
+        });
     }
 }
